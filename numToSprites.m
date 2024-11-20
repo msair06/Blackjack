92d38633795e1b25013f975e165f1d153c76dac1
HelloWorldSQ7 <spencerqin@outlook.com>
2024-11-20 14:09:46 -0500

    Add "Restart" function
    1. Add "Restart" function
    2. Change "0" position form 11 to 91

--- numToSprites.m
+++ numToSprites.m
@@ -1,7 +1,7 @@
 function spriteArray = numToSprites(num)
 numArray = num2str(num) - '0'; % Split the number into an array of numbers
 spriteArray = zeros(1, width(numArray)); %preallocate array for speed
-numberSprites = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]; % each index corresponds to the proper numeral sprite on the sprite sheet.
+numberSprites = [91, 12, 13, 14, 15, 16, 17, 18, 19, 20]; % each index corresponds to the proper numeral sprite on the sprite sheet.
 for i = 1:width(numArray)
     spriteArray(i) = numberSprites(numArray(i)+1);
 end
